df48d674f9dd639ad23431448923ff1ee7546148
Nathalia Zabeu <277927071+nbzabeu-coder@users.noreply.github.com>
2026-04-30 16:46:33 -0300

    Adiciona configuração Maven e classe principal (Tarefa 3)

---
+++ src/main/java/com/gestaoprojetos/App.java
@@ -0,0 +1,8 @@
+package com.gestaoprojetos;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de Gestão de Projetos e Equipes — inicializando...");
+    }
+}
